99e6f5898a89917a1c9a7b5aa5e6fd2df03e24b6
Anthony1441 <anthonynavarrette2017@gmail.com>
2023-01-05 18:31:32 -0800

    Don't need to create team_spread column

--- Data Prep.R
+++ Data Prep.R
@@ -10,7 +10,7 @@ library(nflreadr)
 schedules_clean <- clean_homeaway(load_schedules(seasons = 2010:2022), 
                                   invert = c("result","spread_line"))
 
-### build ats data
+### build ats results data
 ats_df <- schedules_clean |> 
   group_by(game_id, team) |> 
   slice_head(n = 1) |> 
@@ -19,13 +19,6 @@ ats_df <- schedules_clean |>
     team_score > opponent_score ~ 1, 
     team_score == opponent_score ~ 0.5, 
     team_score < opponent_score ~ 0
-  )) |> 
-  mutate(team_spread = case_when(
-    spread_line <= -10 ~ "a", 
-    spread_line < -3 ~ "b", 
-    spread_line <= 3 ~ "c", 
-    spread_line <= 9.9 ~ "d", 
-    spread_line >= 10 ~ "e", 
   )) |> 
   mutate(team_cover = case_when(
     result > spread_line ~ 1, 
