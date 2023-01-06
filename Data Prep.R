e26dec2ee72b1f2548afa882ffadade7ded7fd2d
Anthony1441 <anthonynavarrette2017@gmail.com>
2023-01-05 18:17:03 -0800

    Loaded and Cleaned Schedules

--- Data Prep.R
+++ Data Prep.R
@@ -1,5 +1,13 @@
 library(tidyverse)
 library(nflreadr)
 
-# load schedules
+# Load Data ---------------------------------------------------------------
+
+
+# schedules
+
+### load schedules data
 schedules <- load_schedules(seasons = 2010:2022)
+
+### clean it
+schedules_clean <- clean_homeaway(schedules, invert = c("result","spread_line"))
