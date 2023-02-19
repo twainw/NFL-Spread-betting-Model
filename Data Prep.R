ce59f8d568529931d27cf77b0c6cb020b425f800
Anthony1441 <anthonynavarrette2017@gmail.com>
2023-02-19 11:30:56 -0800

    Expanded seasons to 2007-2022, and created the modeling dataset with ranks and schedule info.

--- Data Prep.R
+++ Data Prep.R
@@ -4,13 +4,36 @@ library(slider)
 
 # Prepare Data ------------------------------------------------------------
 
-seasons <- 2010:2022
+seasons <- 2007:2022
+
+## preliminary calculations
+
+### number of rows the model data should have
+(32*16*(length(seasons)-2))+(32*17*2)-2
+
+### number of missing values in the rolling averages column
+32*(n+1)*length(seasons)
+
+### % missing
+(32*(n+1)*length(seasons)) / ((32*16*(length(seasons)-2))+(32*17*2)-2) * 100
+
+### number of rows left after removing missing values
+((32*16*(length(seasons)-2))+(32*17*2)-2) - 32*(n+1)*length(seasons)
+
+### increasing the number of seasons won't make a difference to the % missing unless n changes.
+### but will have more games to build and test the model.
 
 ## schedules
-schedules <- load_schedules(seasons = seasons)
+schedules <- load_schedules(seasons = seasons) |> 
+  mutate(away_team = clean_team_abbrs(away_team),
+         home_team = clean_team_abbrs(home_team))
 
+schedules_long <- schedules |> 
+  select(game_id, away_team, home_team) |> 
+  pivot_longer(!game_id, names_to = "type", values_to = "team")
+  
 ## EPA and total plays
-pbp <- load_pbp(seasons = seasons)
+pbp <- load_pbp(seasons = seasons) |> filter(season_type == 'REG')
 
 ### calculate rushing and passing avg EPA for each gameID, and posteam
 off_epa <- pbp |> 
@@ -67,21 +90,55 @@ epa_df <- off_epa |>
 ## we don't want to include week 6 epa cause that will be unknown going into week 6.
 ## thus, n=4. Thus, week 5 roll avg will include week 5 + epa in the previous 4 (n) games.
 ## than we lag this result to get the first 5 games avg epa for week 6
-n=4 
+## or the previous five game average going into any week after wk 6.
 
-## should use the across function to apply this to all other columns
-## avoid manually typing them out
-epa_df |> 
-  select(game_id:total_rush_plays) |>
+n=3
+
+## calculate the previous 5 game average of the efficiency metrics
+roll_epa_df <- epa_df |> 
   group_by(season, team) |> 
   mutate(
-    rolling_rush_epa_per_play = lag(slide_sum(total_rush_epa, before=n, complete = T) /
-      slide_sum(total_rush_plays, before=n, complete = T))
+    
+    ### rush EPA
+    rolling_rush_epa_avg = lag(slide_sum(total_rush_epa, before = n, complete = T)/
+                                 slide_sum(total_rush_plays, before = n, complete = T)),
+    ### pass EPA
+    rolling_pass_epa_avg = lag(slide_sum(total_pass_epa, before = n, complete = T) /
+                                 slide_sum(total_pass_plays, before = n, complete = T)),
+    
+    ### rush def EPA
+    rolling_rush_def_epa_avg = lag(slide_sum(total_rush_def_epa, before = n, complete = T) /
+                                     slide_sum(total_rush_def_plays, before = n, complete = T)),
+    
+    ### pass def EPA
+    rolling_pass_def_epa_avg = lag(slide_sum(total_pass_def_epa, before = n, complete = T) /
+                                     slide_sum(total_pass_def_plays, before = n, complete = T))
       ) |> 
-  arrange(season, team, week) |> 
-  View()
+  arrange(season, team, week)
 
+## check missing values
+sapply(roll_epa_df, function(x) sum(is.na(x))) # <- actual missing
+32*(n+1)*length(seasons) # <- expected missing
+((32*16*(length(seasons)-2))+(32*17*2)-2) - 32*(n+1)*length(seasons) # <- expected number of rows after removing NAs
 
+## calculate ranks
+final_df <- roll_epa_df |> 
+  filter(!is.na(rolling_rush_epa_avg)) |> 
+  group_by(season, week) |> 
+  mutate(rolling_rush_epa_avg_rank = rank(desc(rolling_rush_epa_avg), ties.method = 'average'),
+         rolling_pass_epa_avg_rank = rank(desc(rolling_pass_epa_avg), ties.method = 'average'),
+         rolling_rush_def_epa_avg_rank = rank(rolling_rush_def_epa_avg, ties.method = 'average'),
+         rolling_pass_def_epa_avg_rank = rank(rolling_pass_def_epa_avg, ties.method = 'average')
+         ) |> 
+  ungroup() |>
+  left_join(schedules_long, by = c("game_id", "team")) |> 
+  relocate(type, .after = team) |>
+  select(game_id, type, total_rush_epa:last_col()) |>
+  pivot_wider(names_from = type, 
+              values_from = c(total_rush_epa:last_col())) |> 
+  left_join(schedules |> select(game_id, weekday, away_team, home_team, spread_line, 
+                                div_game, roof, surface, away_coach, home_coach, stadium), 
+            by = "game_id")
 
 
 
